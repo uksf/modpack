968d064f5524721549a2363cb65a5f216e197c82
Tim Beswick <contact.tim.here@gmail.com>
2017-02-19 15:01:37 +0000

    Whitespace

--- addons/gear/CfgVehiclesPrivate.hpp
+++ addons/gear/CfgVehiclesPrivate.hpp
@@ -93,40 +93,40 @@ class CUP_Weapon_arifle_L86A2_ACOG : CUP_Weapons_AssaultRifles_Base {
 
 class CUP_L111A1_base;
 class CUP_B_L111A1_BAF_MPT: CUP_L111A1_base {
-	scope = 0;
-	scopeCurator = 0;
+    scope = 0;
+    scopeCurator = 0;
 };
 class CUP_B_L111A1_BAF_DDPM: CUP_L111A1_base{
-	scope = 0;
-	scopeCurator = 0;
+    scope = 0;
+    scopeCurator = 0;
 };
 class CUP_B_L111A1_BAF_WDL: CUP_L111A1_base {
-	scope = 0;
-	scopeCurator = 0;
+    scope = 0;
+    scopeCurator = 0;
 };
 class CUP_L111A1_MiniTripod_base;
 class CUP_B_L111A1_MiniTripod_BAF_MPT: CUP_L111A1_MiniTripod_base {
-	scope = 0;
-	scopeCurator = 0;
+    scope = 0;
+    scopeCurator = 0;
 };
 class CUP_B_L111A1_MiniTripod_BAF_DDPM: CUP_L111A1_MiniTripod_base {
-	scope = 0;
-	scopeCurator = 0;
+    scope = 0;
+    scopeCurator = 0;
 };
 class CUP_B_L111A1_MiniTripod_BAF_WDL: CUP_L111A1_MiniTripod_base {
-	scope = 0;
-	scopeCurator = 0;
+    scope = 0;
+    scopeCurator = 0;
 };
 class CUP_L16A2_base;
 class CUP_B_L16A2_BAF_MPT: CUP_L16A2_base {
-	scope = 0;
-	scopeCurator = 0;
+    scope = 0;
+    scopeCurator = 0;
 };
 class CUP_B_L16A2_BAF_DDPM: CUP_L16A2_base {
-	scope = 0;
-	scopeCurator = 0;
+    scope = 0;
+    scopeCurator = 0;
 };
 class CUP_B_L16A2_BAF_WDL: CUP_L16A2_base {
-	scope = 0;
-	scopeCurator = 0;
+    scope = 0;
+    scopeCurator = 0;
 };
