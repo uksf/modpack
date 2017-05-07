df3317496e73c473fd4806dcbd1dd7dc37f2f566
Tim Beswick <contact.tim.here@gmail.com>
2017-05-07 16:43:27 +0100

    Formatting

--- addons/units/units/CfgCivs.hpp
+++ addons/units/units/CfgCivs.hpp
@@ -1,20 +1,21 @@
 //Middle Eastern Civs
-
-class UKSF_TK_Civ_Sheikh: C_man_p_fugitive_F {
-        uniformClass = "CUP_O_TKI_Khet_Partug_03";
-        headgearList[] = {"CUP_H_TKI_Lungee_Open_05"};
-        displayName = "Sheikh";
-        faction = "CUP_C_TK";
-    };
-class UKSF_TK_Civ_Mullah: C_man_p_fugitive_F {
-        uniformClass = "CUP_O_TKI_Khet_Partug_03";
-        headgearList[] = {"CUP_H_TKI_Lungee_Open_04"};
-        displayName = "Mullah";
-        faction = "CUP_C_TK";
-    };
-class C_man_p_fugitive_F: C_man_p_fugitive_F {
-        uniformClass = "CUP_O_TKI_Khet_Partug_01";
-        headgearList[] = {"CUP_H_TKI_Lungee_Open_01",
+class C_man_p_fugitive_F;
+class UKSF_TK_Civ_Sheikh : C_man_p_fugitive_F {
+    uniformClass = "CUP_O_TKI_Khet_Partug_03";
+    headgearList[] = { "CUP_H_TKI_Lungee_Open_05" };
+    displayName = "Sheikh";
+    faction = "CUP_C_TK";
+};
+class UKSF_TK_Civ_Mullah : C_man_p_fugitive_F {
+    uniformClass = "CUP_O_TKI_Khet_Partug_03";
+    headgearList[] = { "CUP_H_TKI_Lungee_Open_04" };
+    displayName = "Mullah";
+    faction = "CUP_C_TK";
+};
+class C_man_p_fugitive_F : C_man_p_fugitive_F {
+    uniformClass = "CUP_O_TKI_Khet_Partug_01";
+    headgearList[] = { 
+        "CUP_H_TKI_Lungee_Open_01",
         "CUP_H_TKI_Lungee_Open_02",
         "CUP_H_TKI_Lungee_Open_03",
         "CUP_H_TKI_Lungee_Open_06",
@@ -28,13 +29,14 @@ class C_man_p_fugitive_F: C_man_p_fugitive_F {
         "CUP_H_TKI_SkullCap_04",
         "CUP_H_TKI_SkullCap_05",
         "CUP_H_TKI_SkullCap_06"
-        };
-        displayName = "TakiCiv1";
-        faction = "CUP_C_TK";
     };
-class UKSF_TK_Civ_2: C_man_p_fugitive_F {
-        uniformClass = "CUP_O_TKI_Khet_Partug_02";
-        headgearList[] = {"CUP_H_TKI_Lungee_Open_01",
+    displayName = "TakiCiv1";
+    faction = "CUP_C_TK";
+};
+class UKSF_TK_Civ_2 : C_man_p_fugitive_F {
+    uniformClass = "CUP_O_TKI_Khet_Partug_02";
+    headgearList[] = { 
+        "CUP_H_TKI_Lungee_Open_01",
         "CUP_H_TKI_Lungee_Open_02",
         "CUP_H_TKI_Lungee_Open_03",
         "CUP_H_TKI_Lungee_Open_06",
@@ -48,13 +50,14 @@ class UKSF_TK_Civ_2: C_man_p_fugitive_F {
         "CUP_H_TKI_SkullCap_04",
         "CUP_H_TKI_SkullCap_05",
         "CUP_H_TKI_SkullCap_06"
-        };
-        displayName = "TakiCiv2";
-        faction = "CUP_C_TK";
     };
-class UKSF_TK_Civ_3: C_man_p_fugitive_F {
-        uniformClass = "CUP_O_TKI_Khet_Partug_04";
-        headgearList[] = {"CUP_H_TKI_Lungee_Open_01",
+    displayName = "TakiCiv2";
+    faction = "CUP_C_TK";
+};
+class UKSF_TK_Civ_3 : C_man_p_fugitive_F {
+    uniformClass = "CUP_O_TKI_Khet_Partug_04";
+    headgearList[] = { 
+        "CUP_H_TKI_Lungee_Open_01",
         "CUP_H_TKI_Lungee_Open_02",
         "CUP_H_TKI_Lungee_Open_03",
         "CUP_H_TKI_Lungee_Open_06",
@@ -68,13 +71,14 @@ class UKSF_TK_Civ_3: C_man_p_fugitive_F {
         "CUP_H_TKI_SkullCap_04",
         "CUP_H_TKI_SkullCap_05",
         "CUP_H_TKI_SkullCap_06"
-        };
-        displayName = "TakiCiv3";
-        faction = "CUP_C_TK";
     };
-class UKSF_TK_Civ_4: C_man_p_fugitive_F {
-        uniformClass = "CUP_O_TKI_Khet_Partug_05";
-        headgearList[] = {"CUP_H_TKI_Lungee_Open_01",
+    displayName = "TakiCiv3";
+    faction = "CUP_C_TK";
+};
+class UKSF_TK_Civ_4 : C_man_p_fugitive_F {
+    uniformClass = "CUP_O_TKI_Khet_Partug_05";
+    headgearList[] = { 
+        "CUP_H_TKI_Lungee_Open_01",
         "CUP_H_TKI_Lungee_Open_02",
         "CUP_H_TKI_Lungee_Open_03",
         "CUP_H_TKI_Lungee_Open_06",
@@ -88,13 +92,14 @@ class UKSF_TK_Civ_4: C_man_p_fugitive_F {
         "CUP_H_TKI_SkullCap_04",
         "CUP_H_TKI_SkullCap_05",
         "CUP_H_TKI_SkullCap_06"
-        };
-        displayName = "TakiCiv4";
-        faction = "CUP_C_TK";
     };
-class UKSF_TK_Civ_5: C_man_p_fugitive_F {
-        uniformClass = "CUP_O_TKI_Khet_Partug_06";
-        headgearList[] = {"CUP_H_TKI_Lungee_Open_01",
+    displayName = "TakiCiv4";
+    faction = "CUP_C_TK";
+};
+class UKSF_TK_Civ_5 : C_man_p_fugitive_F {
+    uniformClass = "CUP_O_TKI_Khet_Partug_06";
+    headgearList[] = { 
+        "CUP_H_TKI_Lungee_Open_01",
         "CUP_H_TKI_Lungee_Open_02",
         "CUP_H_TKI_Lungee_Open_03",
         "CUP_H_TKI_Lungee_Open_06",
@@ -108,13 +113,14 @@ class UKSF_TK_Civ_5: C_man_p_fugitive_F {
         "CUP_H_TKI_SkullCap_04",
         "CUP_H_TKI_SkullCap_05",
         "CUP_H_TKI_SkullCap_06"
-        };
-        displayName = "TakiCiv5";
-        faction = "CUP_C_TK";
     };
-class UKSF_TK_Civ_6: C_man_p_fugitive_F {
-        uniformClass = "CUP_O_TKI_Khet_Partug_07";
-        headgearList[] = {"CUP_H_TKI_Lungee_Open_01",
+    displayName = "TakiCiv5";
+    faction = "CUP_C_TK";
+};
+class UKSF_TK_Civ_6 : C_man_p_fugitive_F {
+    uniformClass = "CUP_O_TKI_Khet_Partug_07";
+    headgearList[] = { 
+        "CUP_H_TKI_Lungee_Open_01",
         "CUP_H_TKI_Lungee_Open_02",
         "CUP_H_TKI_Lungee_Open_03",
         "CUP_H_TKI_Lungee_Open_06",
@@ -128,13 +134,14 @@ class UKSF_TK_Civ_6: C_man_p_fugitive_F {
         "CUP_H_TKI_SkullCap_04",
         "CUP_H_TKI_SkullCap_05",
         "CUP_H_TKI_SkullCap_06"
-        };
-        displayName = "TakiCiv6";
-        faction = "CUP_C_TK";
     };
-class UKSF_TK_Civ_7: C_man_p_fugitive_F {
-        uniformClass = "CUP_O_TKI_Khet_Partug_08";
-        headgearList[] = {"CUP_H_TKI_Lungee_Open_01",
+    displayName = "TakiCiv6";
+    faction = "CUP_C_TK";
+};
+class UKSF_TK_Civ_7 : C_man_p_fugitive_F {
+    uniformClass = "CUP_O_TKI_Khet_Partug_08";
+    headgearList[] = { 
+        "CUP_H_TKI_Lungee_Open_01",
         "CUP_H_TKI_Lungee_Open_02",
         "CUP_H_TKI_Lungee_Open_03",
         "CUP_H_TKI_Lungee_Open_06",
@@ -148,7 +155,7 @@ class UKSF_TK_Civ_7: C_man_p_fugitive_F {
         "CUP_H_TKI_SkullCap_04",
         "CUP_H_TKI_SkullCap_05",
         "CUP_H_TKI_SkullCap_06"
-        };
-        displayName = "TakiCiv7";
-        faction = "CUP_C_TK";
     };
+    displayName = "TakiCiv7";
+    faction = "CUP_C_TK";
+};
