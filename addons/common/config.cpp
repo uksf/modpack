88f86fed8d7fba1af8e6caa6a27d339654a6b87e
Tim Beswick <contact.tim.here@gmail.com>
2017-09-22 17:44:49 +0100

    Added module to patches

--- addons/common/config.cpp
+++ addons/common/config.cpp
@@ -4,6 +4,7 @@ class CfgPatches {
     class ADDON {
         name = COMPONENT_NAME;
         units[] = {
+            "VCOM_SupportToggle",
             "UKSFModuleRespawnPosition_F"
         };
         weapons[] = {};
