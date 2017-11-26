bbca048ea92e96db8b732360be58d999921cbea9
Tim Beswick <contact.tim.here@gmail.com>
2017-11-26 17:19:23 +0000

    Disabled support function EH

--- addons/mission/CfgEventHandlers.hpp
+++ addons/mission/CfgEventHandlers.hpp
@@ -16,8 +16,8 @@ class Extended_PostInit_EventHandlers {
     };
 };
 
-class Extended_Init_Eventhandlers {
+/*class Extended_Init_Eventhandlers {
     class CAManBase {
         init = QUOTE((_this select 0) addMPEventHandler [ARR_2('MPHit',{_this call FUNC(support)})]);
     };
-};
+};*/
