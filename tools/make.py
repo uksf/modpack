27111d6945bc8bd9fca882577512aa758e92698c
Tim Beswick <contact.tim.here@gmail.com>
2018-11-27 17:03:43 +0000

    Edited make to include intercept dependencies

--- tools/make.py
+++ tools/make.py
@@ -72,10 +72,8 @@ prefix = "uksf"
 pbo_name_prefix = "uksf_"
 dependencies = "C:/SteamLibrary/_Working/next/@uksf_dependencies/addons"
 signature_blacklist = []
-importantFiles = ["mod.cpp", "README.md", "mod.paa", "modLarge.paa", "AUTHORS.txt", "LICENSE",
-                  "UKSFTemplate.VR", "cba_settings.sqf", "cba_settings.sqf"]
-interceptFiles = ["uksf.dll", "uksf_x64.dll", "PocoFoundation.dll",
-                  "PocoFoundation64.dll", "PocoNet.dll", "PocoNet64.dll"]
+importantFiles = ["mod.cpp", "README.md", "mod.paa", "modLarge.paa", "AUTHORS.txt", "LICENSE", "UKSFTemplate.VR", "cba_settings.sqf", "cba_settings.sqf"]
+interceptFiles = ["uksf.dll", "uksf_x64.dll", "PocoFoundation.dll", "PocoFoundation64.dll", "PocoJSON.dll", "PocoJSON64.dll", "PocoNet.dll", "PocoNet64.dll", "PocoUtil.dll", "PocoUtil64.dll", "PocoXML.dll", "PocoXML64.dll"]
 versionFiles = ["mod.cpp", "README.md"]
 
 ###############################################################################
