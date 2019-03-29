69f2cad560380df83313ecc7d011759746283b74
AnAngrySalad1 <bridgforda@hotmail.co.uk>
2019-03-29 20:53:34 +0000

    Update CfgGroups.hpp

--- addons/pror/CfgGroups.hpp
+++ addons/pror/CfgGroups.hpp
@@ -28,13 +28,13 @@ class CfgGroups {
                         position[] = { -5, -5, 0 };
                         rank = "PRIVATE";
                         side = 0;
-                        vehicle = "PROR_Rfn";
+                        vehicle = "PROR_r";
                     };
                     class Unit3 {
                         position[] = { 10, -10, 0 };
                         rank = "PRIVATE";
                         side = 0;
-                        vehicle = "PROR_Rfn";
+                        vehicle = "PROR_r";
                     };
                     class Unit4 {
                         position[] = { -10, -10, 0 };
@@ -46,19 +46,343 @@ class CfgGroups {
                         position[] = { 15, -15, 0 };
                         rank = "CORPORAL";
                         side = 0;
-                        vehicle = "PROR_Rfn";
+                        vehicle = "PROR_Medic";
                     };
                     class Unit6 {
                         position[] = { -15, -15, 0 };
                         rank = "PRIVATE";
                         side = 0;
-                        vehicle = "PROR_AR";
+                        vehicle = "PROR_Marks";
                     };
                     class Unit7 {
                         position[] = { 20, -20, 0 };
                         rank = "PRIVATE";
                         side = 0;
-                        vehicle = "PROR_Rfn";
+                        vehicle = "PROR_MG";
+                    };
+                };
+				
+				class PROR_MGSection {
+                    faction = "UKSF_PROR";
+                    name = "MG Section";
+                    side = 0;
+                    class Unit0 {
+                        position[] = { 0, 0, 0 };
+                        rank = "SERGEANT";
+                        side = 0;
+                        vehicle = "PROR_AR";
+                    };
+                    class Unit1 {
+                        position[] = { 5, -5, 0 };
+                        rank = "CORPORAL";
+                        side = 0;
+                        vehicle = "PROR_SL";
+                    };
+                    class Unit2 {
+                        position[] = { -5, -5, 0 };
+                        rank = "PRIVATE";
+                        side = 0;
+                        vehicle = "PROR_MG";
+                    };
+                    class Unit3 {
+                        position[] = { 10, -10, 0 };
+                        rank = "PRIVATE";
+                        side = 0;
+                        vehicle = "PROR_r";
+                    };
+                    class Unit4 {
+                        position[] = { -10, -10, 0 };
+                        rank = "PRIVATE";
+                        side = 0;
+                        vehicle = "PROR_MG";
+                    };
+                    class Unit5 {
+                        position[] = { 15, -15, 0 };
+                        rank = "CORPORAL";
+                        side = 0;
+                        vehicle = "PROR_r";
+                    };
+                    class Unit6 {
+                        position[] = { -15, -15, 0 };
+                        rank = "PRIVATE";
+                        side = 0;
+                        vehicle = "PROR_MG";
+                    };
+                    class Unit7 {
+                        position[] = { 20, -20, 0 };
+                        rank = "PRIVATE";
+                        side = 0;
+                        vehicle = "PROR_r";
+                    };
+                };
+				
+				class PROR_ATSection {
+                    faction = "UKSF_PROR";
+                    name = "AT Section";
+                    side = 0;
+                    class Unit0 {
+                        position[] = { 0, 0, 0 };
+                        rank = "SERGEANT";
+                        side = 0;
+                        vehicle = "PROR_r";
+                    };
+                    class Unit1 {
+                        position[] = { 5, -5, 0 };
+                        rank = "CORPORAL";
+                        side = 0;
+                        vehicle = "PROR_SL";
+                    };
+                    class Unit2 {
+                        position[] = { -5, -5, 0 };
+                        rank = "PRIVATE";
+                        side = 0;
+                        vehicle = "PROR_AT_H";
+                    };
+                    class Unit3 {
+                        position[] = { 10, -10, 0 };
+                        rank = "PRIVATE";
+                        side = 0;
+                        vehicle = "PROR_r";
+                    };
+                    class Unit4 {
+                        position[] = { -10, -10, 0 };
+                        rank = "PRIVATE";
+                        side = 0;
+                        vehicle = "PROR_AT_H";
+                    };
+                    class Unit5 {
+                        position[] = { 15, -15, 0 };
+                        rank = "CORPORAL";
+                        side = 0;
+                        vehicle = "PROR_AT_L";
+                    };
+                    class Unit6 {
+                        position[] = { -15, -15, 0 };
+                        rank = "PRIVATE";
+                        side = 0;
+                        vehicle = "PROR_AT_L";
+                    };
+                    class Unit7 {
+                        position[] = { 20, -20, 0 };
+                        rank = "PRIVATE";
+                        side = 0;
+                        vehicle = "PROR_MG";
+                    };
+                };
+				
+				class PROR_AATeam {
+                    faction = "UKSF_PROR";
+                    name = "AA Team";
+                    side = 0;
+                    class Unit0 {
+                        position[] = { 0, 0, 0 };
+                        rank = "SERGEANT";
+                        side = 0;
+                        vehicle = "PROR_AA";
+                    };
+                    class Unit1 {
+                        position[] = { 5, -5, 0 };
+                        rank = "CORPORAL";
+                        side = 0;
+                        vehicle = "PROR_AA";
+                    };
+                    class Unit2 {
+                        position[] = { -5, -5, 0 };
+                        rank = "PRIVATE";
+                        side = 0;
+                        vehicle = "PROR_AA";
+                    };
+                };
+				
+				class PROR_PARA_Section {
+                    faction = "UKSF_PROR";
+                    name = "PARA Section";
+                    side = 0;
+                    class Unit0 {
+                        position[] = { 0, 0, 0 };
+                        rank = "SERGEANT";
+                        side = 0;
+                        vehicle = "PROR_Para_AR";
+                    };
+                    class Unit1 {
+                        position[] = { 5, -5, 0 };
+                        rank = "CORPORAL";
+                        side = 0;
+                        vehicle = "PROR_Para_SL";
+                    };
+                    class Unit2 {
+                        position[] = { -5, -5, 0 };
+                        rank = "PRIVATE";
+                        side = 0;
+                        vehicle = "PROR_Para";
+                    };
+                    class Unit3 {
+                        position[] = { 10, -10, 0 };
+                        rank = "PRIVATE";
+                        side = 0;
+                        vehicle = "PROR_Para";
+                    };
+                    class Unit4 {
+                        position[] = { -10, -10, 0 };
+                        rank = "PRIVATE";
+                        side = 0;
+                        vehicle = "PROR_Para_Marks";
+                    };
+                    class Unit5 {
+                        position[] = { 15, -15, 0 };
+                        rank = "CORPORAL";
+                        side = 0;
+                        vehicle = "PROR_Para_AT";
+                    };
+                };
+				
+				class PROR_Sentry {
+                    faction = "UKSF_PROR";
+                    name = "Sentry";
+                    side = 0;
+                    class Unit0 {
+                        position[] = { 0, 0, 0 };
+                        rank = "SERGEANT";
+                        side = 0;
+                        vehicle = "PROR_AR";
+                    };
+                    class Unit1 {
+                        position[] = { 5, -5, 0 };
+                        rank = "CORPORAL";
+                        side = 0;
+                        vehicle = "PROR_SL";
+                    };
+                };
+				
+				class PROR_Fireteam {
+                    faction = "UKSF_PROR";
+                    name = "Fireteam";
+                    side = 0;
+                    class Unit0 {
+                        position[] = { 0, 0, 0 };
+                        rank = "SERGEANT";
+                        side = 0;
+                        vehicle = "PROR_AR";
+                    };
+                    class Unit1 {
+                        position[] = { 5, -5, 0 };
+                        rank = "CORPORAL";
+                        side = 0;
+                        vehicle = "PROR_SL";
+                    };
+                    class Unit2 {
+                        position[] = { -5, -5, 0 };
+                        rank = "PRIVATE";
+                        side = 0;
+                        vehicle = "PROR_AT_L";
+                    };
+                    class Unit3 {
+                        position[] = { 10, -10, 0 };
+                        rank = "PRIVATE";
+                        side = 0;
+                        vehicle = "PROR_r";
+                    };
+                };
+				
+				class PROR_SniperPair {
+                    faction = "UKSF_PROR";
+                    name = "Sniper Pair";
+                    side = 0;
+                    class Unit0 {
+                        position[] = { 0, 0, 0 };
+                        rank = "SERGEANT";
+                        side = 0;
+                        vehicle = "PROR_Sniper";
+                    };
+                    class Unit1 {
+                        position[] = { 5, -5, 0 };
+                        rank = "CORPORAL";
+                        side = 0;
+                        vehicle = "PROR_Sniper";
+                    };
+                };
+				
+				class PROR_SFTeam {
+                    faction = "UKSF_PROR";
+                    name = "SF Team";
+                    side = 0;
+                    class Unit0 {
+                        position[] = { 0, 0, 0 };
+                        rank = "SERGEANT";
+                        side = 0;
+                        vehicle = "PROR_SF_R";
+                    };
+                    class Unit1 {
+                        position[] = { 5, -5, 0 };
+                        rank = "CORPORAL";
+                        side = 0;
+                        vehicle = "PROR_SF_TL";
+                    };
+                    class Unit2 {
+                        position[] = { -5, -5, 0 };
+                        rank = "PRIVATE";
+                        side = 0;
+                        vehicle = "PROR_SF_AR";
+                    };
+                    class Unit3 {
+                        position[] = { -5, -5, 0 };
+                        rank = "PRIVATE";
+                        side = 0;
+                        vehicle = "PROR_SF_Marks";
+                    };
+					class Unit4 {
+                        position[] = { -5, -5, 0 };
+                        rank = "PRIVATE";
+                        side = 0;
+                        vehicle = "PROR_SF_Med";
+                    };
+					class Unit5 {
+                        position[] = { 0, 0, 0 };
+                        rank = "PRIVATE";
+                        side = 0;
+                        vehicle = "PROR_SF_R";
+                    };
+                };
+				
+				class PROR_HQSection {
+                    faction = "UKSF_PROR";
+                    name = "HQ Section";
+                    side = 0;
+                    class Unit0 {
+                        position[] = { 0, 0, 0 };
+                        rank = "SERGEANT";
+                        side = 0;
+                        vehicle = "PROR_Officer";
+                    };
+                    class Unit1 {
+                        position[] = { 5, -5, 0 };
+                        rank = "CORPORAL";
+                        side = 0;
+                        vehicle = "PROR_FOO";
+                    };
+                    class Unit2 {
+                        position[] = { -5, -5, 0 };
+                        rank = "PRIVATE";
+                        side = 0;
+                        vehicle = "PROR_FAC";
+                    };
+                    class Unit3 {
+                        position[] = { -5, -5, 0 };
+                        rank = "PRIVATE";
+                        side = 0;
+                        vehicle = "PROR_Medic";
+                    };
+					class Unit4 {
+                        position[] = { -5, -5, 0 };
+                        rank = "PRIVATE";
+                        side = 0;
+                        vehicle = "PROR_Marks";
+                    };
+					class Unit5 {
+                        position[] = { 0, 0, 0 };
+                        rank = "PRIVATE";
+                        side = 0;
+                        vehicle = "PROR_MG";
                     };
                 };
             };
@@ -233,32 +557,6 @@ class CfgGroups {
             //             vehicle = "Inegal_LandRover_GL";
             //             rank = "PRIVATE";
             //             position[] = { 0, -10, 0 };
-            //         };
-            //     };
-            // };
-            // class SpecOps {
-            //     name = "SpecOps";
-            //     aliveCategory = "SpecOps";
-            // };
-            // class Support {
-            //     name = "Support";
-            //     aliveCategory = "Support";
-            //     class Inegal_TRM {
-            //         name = "TRM Troop";
-            //         faction = "UKSF_Inegal";
-            //         rarityGroup = 0.5;
-            //         side = 0;
-            //         class Unit0 {
-            //             side = 0;
-            //             vehicle = "Inegal_TRM_Covered";
-            //             rank = "LIEUTENANT";
-            //             position[] = { 0, 0, 0 };
-            //         };
-            //         class Unit1 {
-            //             side = 0;
-            //             vehicle = "Inegal_TRM_Open";
-            //             rank = "PRIVATE";
-            //             position[] = { 0, -20, 0 };
             //         };
             //     };
             // };
