16503ede25eeea816f8b7360e5def9f1d1e97480
AnAngrySalad1 <bridgforda@hotmail.co.uk>
2019-04-01 16:37:14 +0100

    Update CfgHeli.hpp

--- addons/pror/vehicles/CfgHeli.hpp
+++ addons/pror/vehicles/CfgHeli.hpp
@@ -1,69 +1,38 @@
-// AW101
-class I_Heli_Transport_02_F;
-class Inegal_Heli_AW101: I_Heli_Transport_02_F {
+// Huey
+class CFP_B_USCIA_UH_1H_01;
+class PROR_Heli_Huey: CFP_B_USCIA_UH_1H_01 {
     scope = 2;
     side = 0;
-    faction = "UKSF_Inegal";
-    displayName = "AW101";
-    editorPreview = QPATHTOEF(common,data\previews\Inegal_Heli_AW101.jpg);
-    crew = "Inegal_Crew_HP";
+    faction = "UKSF_PROR";
+    displayName = "Huey";
+    editorPreview = "\x\cfp\addons\b_uscia\data\preview\CFP_B_USCIA_UH_1H_01.JPG";
+    crew = "PROR_Heli_Pilot";
     typicalCargo[] = {
-        "Inegal_Crew_HC"
+        "PROR_Heli_Pilot"
     };
-    hiddenSelections[] = { "camo1", "camo2", "camo3" };
-    hiddenSelectionsTextures[] = { QPATHTOF(data\blu_mohawk_co.paa), QPATHTOF(data\blu_mohawk2_co.paa), QPATHTOF(data\blu_mohawk3_co.paa) };
+    
     class TransportMagazines {};
     class TransportItems {};
     class TransportWeapons {};
     class TransportBackpacks {};
 };
 
-// AH6
-class B_Heli_Light_01_armed_F;
-class Inegal_Heli_AH6: B_Heli_Light_01_armed_F {
+// Chinook
+class B_Heli_Transport_03_unarmed_F;
+class PROR_Heli_Chinook: B_Heli_Transport_03_unarmed_F {
     scope = 2;
     side = 0;
-    faction = "UKSF_Inegal";
-    displayName = "AH-6";
-    editorPreview = QPATHTOEF(common,data\previews\Inegal_Heli_AH6.jpg);
-    crew = "Inegal_Crew_HP";
+    faction = "UKSF_PROR";
+    displayName = "Chinook";
+    editorPreview = "\A3\EditorPreviews_F\Data\CfgVehicles\B_Heli_Transport_03_unarmed_F.jpg";
+	hiddenSelections[] = {"camo1","camo2"};
+	hiddenSelectionsTextures[] = {"\A3\Air_F_Heli\Heli_Transport_03\Data\Heli_Transport_03_ext01_black_CO.paa","\a3\air_f_heli\heli_transport_03\data\heli_transport_03_ext02_black_co.paa"};
+    crew = "PROR_Heli_Pilot";
     typicalCargo[] = {
-        "Inegal_Crew_HC"
+        "PROR_Heli_Pilot"
     };
     class TransportMagazines {};
     class TransportItems {};
     class TransportWeapons {};
     class TransportBackpacks {};
-};
-
-// MH6
-class B_Heli_Light_01_F;
-class Inegal_Heli_MH6: B_Heli_Light_01_F {
-    scope = 2;
-    side = 0;
-    faction = "UKSF_Inegal";
-    displayName = "MH-6";
-    editorPreview = QPATHTOEF(common,data\previews\Inegal_Heli_MH6.jpg);
-    crew = "Inegal_Crew_HP";
-    typicalCargo[] = {
-        "Inegal_Crew_HC"
-    };
-    class TransportMagazines {};
-    class TransportItems {};
-    class TransportWeapons {};
-    class TransportBackpacks {};
-};
-class Inegal_Heli_FDF_MH6: Inegal_Heli_MH6 {
-    displayName = "MH-6 (FDF)";
-    crew = "Inegal_Crew_HP";
-    typicalCargo[] = {
-        "Inegal_Crew_HC"
-    };
-};
-class Inegal_Heli_D_MH6: Inegal_Heli_MH6 {
-    displayName = "MH-6 (Diablerie)";
-    crew = "Inegal_Crew_HP";
-    typicalCargo[] = {
-        "Inegal_Crew_HC"
-    };
 };
