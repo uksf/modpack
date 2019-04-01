dd27b9ad29991146b5671dbfac1b2c13b2f83cf3
AnAngrySalad1 <bridgforda@hotmail.co.uk>
2019-04-01 16:37:30 +0100

    Update CfgJet.hpp

--- addons/pror/vehicles/CfgJet.hpp
+++ addons/pror/vehicles/CfgJet.hpp
@@ -1,15 +1,14 @@
-// A-149
-class I_Plane_Fighter_04_F;
-class Inegal_Jet_A149: I_Plane_Fighter_04_F {
+// Harrier
+class CUP_B_AV8B_DYN_USMC;
+class PROR_Jet_Harrier: CUP_B_AV8B_DYN_USMC {
     scope = 2;
     side = 0;
-    faction = "UKSF_Inegal";
-    editorPreview = QPATHTOEF(common,data\previews\Inegal_Jet_A149.jpg);
-    crew = "Inegal_Crew_P";
+    faction = "UKSF_PROR";
+    editorPreview = "CUP\AirVehicles\CUP_AirVehicles_Av8B\Data\preview\CUP_B_AV8B_CAP_USMC.jpg";
+    crew = "PROR_Jet_Pilot";
     typicalCargo[] = {
-        "Inegal_Crew_P"
+        "PROR_Jet_Pilot"
     };
-    hiddenSelectionsTextures[] = { "a3\air_f_jets\plane_fighter_04\data\Fighter_04_fuselage_01_co.paa", "a3\air_f_jets\plane_fighter_04\data\Fighter_04_fuselage_02_co.paa", "a3\air_f_jets\plane_fighter_04\data\fighter_04_misc_01_co.paa", "a3\air_f_jets\plane_fighter_04\data\Numbers\Fighter_04_number_04_ca.paa", "a3\air_f_jets\plane_fighter_04\data\Numbers\Fighter_04_number_04_ca.paa", "a3\air_f_jets\plane_fighter_04\data\Numbers\Fighter_04_number_08_ca.paa" };
     class TransportMagazines {};
     class TransportItems {};
     class TransportWeapons {};
