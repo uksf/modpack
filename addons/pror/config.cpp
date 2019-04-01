00b7dfd1b9a62e8897928faf9a2f3c0a99736795
AnAngrySalad1 <bridgforda@hotmail.co.uk>
2019-04-01 16:34:00 +0100

    Update config.cpp

--- addons/pror/config.cpp
+++ addons/pror/config.cpp
@@ -18,7 +18,36 @@ class CfgPatches {
 			"PROR_AT_H",
 
             // Vehicles
-            "PROR_VAB_Unarmed"
+            "PROR_MRAP_Unarmed",
+			"PROR_MRAP_HMG",
+			"PROR_MRAP_GMG",
+			"PROR_Polaris_HMG",
+			"PROR_Striker_GMG",
+			"PROR_Striker_HMG",
+			"PROR_LAV",
+			"PROR_M60",
+			"PROR_AA_Radar",
+			"PROR_AA_Ozelot",
+			"PROR_TRM_Covered",
+			"PROR_TRM_Open",
+			"PROR_TRM_Ammo",
+			"PROR_TRM_Repair",
+			"PROR_TRM_Fuel",
+			
+			// Air
+			"PROR_Jet_Harrier",
+			"PROR_Heli_Huey",
+			"PROR_Heli_Chinook",
+			
+			// Statics
+			"PROR_Static_M119",
+			"PROR_Static_HMG_High",
+			"PROR_Static_HMG_Low",
+			"PROR_Static_GMG_Low",
+			"PROR_Static_GMG_High",
+			"PROR_Static_AA"
+			
+			
         };
         weapons[] = {
             "arifle_CTAR_blk_F_Eotech"
