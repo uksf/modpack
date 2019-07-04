333b16c4d426c1b7b14db41fe0b3ba6b6be0f1ce
AnAngrySalad1 <bridgforda@hotmail.co.uk>
2019-07-04 15:17:49 +0100

    Added CfgFunctions.hpp

---
+++ UKSFTemplate.VR/CfgFunctions.hpp
@@ -0,0 +1,9 @@
+class CfgFunctions {
+	class uksf_RTC {
+        class RTC {
+            file = "functions\RTC";
+            class postInit { postInit = 1; };
+            class getDown {};
+        };
+    };
+};
