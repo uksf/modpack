0458ea0f732d61e172ad77c81b7962555e6d9965
Tim Beswick <contact.tim.here@gmail.com>
2019-10-29 17:26:39 +0000

    Bump version to force key change

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -12,7 +12,7 @@
 #define VERSION_AR  MAJOR,MINOR,PATCHLVL
 
 // MINIMAL required version for the Mod. Components can specify others..
-#define REQUIRED_VERSION 1.94
+#define REQUIRED_VERSION 1.96
 #define REQUIRED_CBA_VERSION {3,12,2}
 
 #ifdef COMPONENT_BEAUTIFIED
