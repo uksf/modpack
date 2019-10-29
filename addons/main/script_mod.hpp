773218ad5352335387250a44b57b195fa0d7e28f
Tim Beswick <contact.tim.here@gmail.com>
2019-10-29 17:56:35 +0000

    5.17.2

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -11,7 +11,7 @@
 #define VERSION_STR MAJOR.MINOR.PATCHLVL
 #define VERSION_AR  MAJOR,MINOR,PATCHLVL
 
-// MINIMAL required version for the Mod. Components can specify others..
+// MINIMAL required version for the Mod. Components can specify others
 #define REQUIRED_VERSION 1.96
 #define REQUIRED_CBA_VERSION {3,12,2}
 
