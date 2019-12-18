57696c3e0a09db1763a0551b1b0ad9b84f108d4f
Tim Beswick <contact.tim.here@gmail.com>
2019-12-18 17:16:06 +0000

    Fixed loadout L85A3 not showing in arsenal

--- addons/arsenal/CfgWeapons.hpp
+++ addons/arsenal/CfgWeapons.hpp
@@ -1,15 +1,19 @@
 class CfgWeapons {
     class CBA_MiscItem;
-    class UK3CB_BAF_Javelin_CLU: CBA_MiscItem {
+    class UK3CB_BAF_Javelin_CLU : CBA_MiscItem {
         scopeArsenal = 2;
     };
     class UK3CB_BAF_L85A2_RIS_AFG;
-    class UK3CB_BAF_L85A2_RIS_AFG_Green: UK3CB_BAF_L85A2_RIS_AFG {
+    class UK3CB_BAF_L85A2_RIS_AFG_Green : UK3CB_BAF_L85A2_RIS_AFG {
         scopeArsenal = 2;
         baseWeapon = "UK3CB_BAF_L85A2_RIS_AFG_Green";
     };
-    class UK3CB_BAF_L85A2_RIS_AFG_Tan: UK3CB_BAF_L85A2_RIS_AFG {
+    class UK3CB_BAF_L85A2_RIS_AFG_Tan : UK3CB_BAF_L85A2_RIS_AFG {
         scopeArsenal = 2;
         baseWeapon = "UK3CB_BAF_L85A2_RIS_AFG_Tan";
+    };
+    class UK3CB_BAF_L85A3_Grippod : UK3CB_BAF_L85A3 {
+        scopeArsenal = 2;
+        baseWeapon = "UK3CB_BAF_L85A3_Grippod";
     };
 };
