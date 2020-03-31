95ddcdb5877aaa802eedfd9d23476d3afc2fae11
Tim Beswick <contact.tim.here@gmail.com>
2020-03-31 02:13:50 +0100

    Adjust chinook engine and rotor sounds to better match startup duration

--- addons/air/vehicles/CfgChinook.hpp
+++ addons/air/vehicles/CfgChinook.hpp
@@ -99,24 +99,24 @@ class CUP_CH47F_base : Helicopter_Base_H {
         class Engine {
             sound[] = { "CUP\AirVehicles\CUP_AirVehicles_CH47\sounds\CH47_engine_high_ext", 2, 1.0, 1000 };
             frequency = "rotorSpeed";
-            volume = "camPos*((rotorSpeed-0.72)*4)";
+            volume = "camPos*((rotorSpeed-0.15)*2)";
         };
         class RotorLowOut {
             sound[] = { "CUP\AirVehicles\CUP_AirVehicles_CH47\sounds\CH47_rotor_ext", 4, 1.0, 2000 };
             frequency = "rotorSpeed";
-            volume = "camPos*(0 max (rotorSpeed-0.1))";
+            volume = "camPos*2*(0 max (rotorSpeed-0.5))";
             cone[] = { 1.8, 3.14, 2.0, 0.9 };
         };
         class RotorHighOut {
             sound[] = { "CUP\AirVehicles\CUP_AirVehicles_CH47\sounds\CH47_rotor_forsage_ext", 4, 1.0, 2500 };
             frequency = "rotorSpeed";
-            volume = "camPos*10*(0 max (rotorThrust-0.95))";
+            volume = "camPos*15*(0 max (rotorThrust-0.95))";
             cone[] = { 1.8, 3.14, 2.0, 0.9 };
         };
         class EngineIn {
             sound[] = { "CUP\AirVehicles\CUP_AirVehicles_CH47\sounds\CH47_engine_high_int", 2, 1.0 };
             frequency = "rotorSpeed";
-            volume = "(1-camPos)*((rotorSpeed-0.75)*4)";
+            volume = "(1-camPos)*((rotorSpeed-0.2)*2)";
         };
         class RotorLowIn {
             sound[] = { "CUP\AirVehicles\CUP_AirVehicles_CH47\sounds\CH47_rotor_int", 2, 1.0 };
