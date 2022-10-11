4827bf869de953636701d80d56da4a6412f420b0
Tim Beswick <contact.tim.here@gmail.com>
2022-10-11 17:42:54 +0100

    Remove restrictions for cup uniforms

---
+++ addons/equipment/config.cpp
@@ -0,0 +1,21 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = COMPONENT_NAME;
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {
+            "uksf_common",
+            "Black_Config",
+            "CUP_Creatures_People_LoadOrder"
+        };
+        author = QUOTE(UKSF);
+        authors[] = { "Beswick.T" };
+        url = URL;
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgVehicles.hpp"
