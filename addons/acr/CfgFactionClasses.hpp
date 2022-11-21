66e5fe484fba595774cf8ef39a9ddb35a56e2dc9
Tim Beswick <contact.tim.here@gmail.com>
2022-11-21 22:07:44 +0000

    Fix flag path

--- addons/acr/CfgFactionClasses.hpp
+++ addons/acr/CfgFactionClasses.hpp
@@ -1,7 +1,7 @@
 class CfgFactionClasses {
     class UKSF_ACR {
         displayName = "ACR";
-        icon = "data\ACR_Flag.png";
+        icon = QPATHTOF(data\ACR_Flag.png);
         priority = 1;
         side = 0;
     };
