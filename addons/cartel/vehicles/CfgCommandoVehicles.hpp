662fa2363e58e64c722ee22ea4b75fe10d8d9cdf
abridgford <bridgforda@hotmail.co.uk>
2023-06-13 20:35:47 +0100

    add LR event handlers

--- addons/cartel/vehicles/CfgCommandoVehicles.hpp
+++ addons/cartel/vehicles/CfgCommandoVehicles.hpp
@@ -1,26 +1,19 @@
 // commando
 class CUP_I_LR_SF_HMG_AAF;
+class EventHandlers;
 class GVAR(commando_LR_MG): CUP_I_LR_SF_HMG_AAF {
     side = 0;
     faction = QUOTE(ADDON);
     displayName = "Commando Land Rover MG";
     hiddenSelectionsTextures[] = { "cup\wheeledvehicles\cup_wheeledvehicles_lr\data\textures\civ_r_lr_base_co.paa","cup\wheeledvehicles\cup_wheeledvehicles_lr\data\textures\civ_r_lr_special_co.paa","cup\wheeledvehicles\cup_wheeledvehicles_lr\data\jackal_base_woodlnd_co.paa","cup\wheeledvehicles\cup_wheeledvehicles_lr\data\jackal_mount_woodlnd_co.paa" };
-    animationList[] = {
-        "selection_tool", 0,
-        "selection_jerry", 1,
-        "selection_antenna", 1,
-        "selection_antenna_rear", 0,
-        "selection_box", 0,
-        "selection_steps", 0,
-        "selection_wheelfront", 0,
-        "selection_wheels", 0,
-        "selection_bar", 0,
-        "selection_cut", 1,
-        "selection_doors", 1
-    };
     crew = QGVAR(commando_hk416);
     typicalCargo[] = {
         QGVAR(commando_hk416);
+    };
+    class EventHandlers: EventHandlers {
+        side = 0;
+        scope = 0;
+        postInit = "if (local (_this select 0)) then { [(_this select 0), nil, ['selection_tool', 0, 'selection_jerry', 1, 'selection_antenna', 1, 'selection_antenna_rear', 0, 'selection_box', 0, 'selection_steps', 0, 'selection_wheelfront', 0, 'selection_wheels', 0, 'selection_bar', 0, 'selection_cut', 1, 'selection_doors', 1]] call BIS_fnc_initVehicle }";
     };
 };
 
@@ -30,23 +23,15 @@ class GVAR(commando_LR_GMG): CUP_B_LR_Special_CZ_W {
     faction = QUOTE(ADDON);
     displayName = "Commando Land Rover GMG";
     hiddenSelectionsTextures[] = { "cup\wheeledvehicles\cup_wheeledvehicles_lr\data\textures\civ_r_lr_base_co.paa","cup\wheeledvehicles\cup_wheeledvehicles_lr\data\textures\civ_r_lr_special_co.paa","cup\wheeledvehicles\cup_wheeledvehicles_lr\data\jackal_base_woodlnd_co.paa","cup\wheeledvehicles\cup_wheeledvehicles_lr\data\jackal_mount_woodlnd_co.paa" };
-    animationList[] = {
-        "selection_tool", 0,
-        "selection_jerry", 1,
-        "selection_antenna", 1,
-        "selection_antenna_rear", 0,
-        "selection_box", 0,
-        "selection_steps", 0,
-        "selection_wheelfront", 0,
-        "selection_wheels", 0,
-        "selection_bar", 0,
-        "selection_cut", 1,
-        "selection_doors", 1
-    };
     crew = QGVAR(commando_hk416);
     typicalCargo[] = {
         QGVAR(commando_hk416);
     };
+    // class EventHandlers: EventHandlers {
+    //     side = 0;
+    //     scope = 0;
+    //     postInit = "if (local (_this select 0)) then { [(_this select 0), nil, ['selection_tool', 0, 'selection_jerry', 1, 'selection_antenna', 1, 'selection_antenna_rear', 0, 'selection_box', 0, 'selection_steps', 0, 'selection_wheelfront', 0, 'selection_wheels', 0, 'selection_bar', 0, 'selection_cut', 1, 'selection_doors', 1]] call BIS_fnc_initVehicle }";
+    // };
 };
 
 class UK3CB_Civ_LandRover_Soft_Green_A;
@@ -70,3 +55,15 @@ class GVAR(commando_van): C_Van_02_transport_F {
         QGVAR(commando_hk416);
     };
 };
+
+        // "selection_tool", 0,
+        // "selection_jerry", 1,
+        // "selection_antenna", 1,
+        // "selection_antenna_rear", 0,
+        // "selection_box", 0,
+        // "selection_steps", 0,
+        // "selection_wheelfront", 0,
+        // "selection_wheels", 0,
+        // "selection_bar", 0,
+        // "selection_cut", 1,
+        // "selection_doors", 1
