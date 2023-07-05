05b3c8d20531235f1b9984a1bace85701f5435d6
Tim Beswick <contact.tim.here@gmail.com>
2023-07-05 20:45:02 +0100

    Add pastel marker colours. Closes #985

--- addons/common/CfgMarkerColors.hpp
+++ addons/common/CfgMarkerColors.hpp
@@ -15,4 +15,12 @@ class CfgMarkerColors {
     class ColorPink: Default {
         scope = 2;
     };
+    MARKER_COLOUR(PastelYellow,"Pastel Yellow",COLOUR_ARRAY(253,253,150));
+    MARKER_COLOUR(PastelGreen,"Pastel Green",COLOUR_ARRAY(201,230,192));
+    MARKER_COLOUR(PastelBlue,"Pastel Blue",COLOUR_ARRAY(119,158,203));
+    MARKER_COLOUR(PastelRed,"Pastel Red",COLOUR_ARRAY(255,105,97));
+    MARKER_COLOUR(PastelOrange,"Pastel Orange",COLOUR_ARRAY(255,179,71));
+    MARKER_COLOUR(PastelPurple,"Pastel Purple",COLOUR_ARRAY(177,156,217));
+    MARKER_COLOUR(PastelBrown,"Pastel Brown",COLOUR_ARRAY(131,105,83));
+    MARKER_COLOUR(PastelPink,"Pastel Pink",COLOUR_ARRAY(222,165,164));
 };
