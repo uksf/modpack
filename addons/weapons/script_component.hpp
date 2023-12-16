9330050f32a7c9a89f4aa4619322dd8d7357879f
Tim Beswick <contact.tim.here@gmail.com>
2023-12-16 01:05:18 +0000

    Fix nvg wp mod reference

--- addons/weapons/script_component.hpp
+++ addons/weapons/script_component.hpp
@@ -7,4 +7,7 @@
 // #define CBA_DEBUG_SYNCHRONOUS
 
 #include "\u\uksf\addons\main\script_macros.hpp"
-#include "\z\ace\addons\nightvision\defines.hpp"
+
+#define NVG_WHITE_PRESET ace_nightvision_colorPreset[] = {0.0, {0.0, 0.0, 0.0, 0.0}, {1.1, 0.8, 1.9, 0.9}, {1, 1, 6, 0.0}}
+
+
