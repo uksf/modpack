91d9f0aef02548eebff610ef51b4ddc864c4cabd
Tim Beswick <contact.tim.here@gmail.com>
2024-04-04 22:35:52 +0100

    Update require Arma & CBA versions

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -12,8 +12,8 @@
 #define VERSION_AR  MAJOR,MINOR,PATCHLVL
 
 // MINIMAL required version for the Mod. Components can specify others
-#define REQUIRED_VERSION 2.10
-#define REQUIRED_CBA_VERSION {3,15,8}
+#define REQUIRED_VERSION 2.14
+#define REQUIRED_CBA_VERSION {3,17,0}
 
 #ifdef COMPONENT_BEAUTIFIED
     #define COMPONENT_NAME QUOTE(UKSF - COMPONENT_BEAUTIFIED)
