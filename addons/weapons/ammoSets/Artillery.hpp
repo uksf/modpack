c00b15c0f99ce16997d86b12ff760ecc79b96002
Tim Beswick <contact.tim.here@gmail.com>
2026-03-11 19:33:44 +0000

    Different cone type

--- addons/weapons/ammoSets/Artillery.hpp
+++ addons/weapons/ammoSets/Artillery.hpp
@@ -34,9 +34,11 @@ class GVAR(105mm_smoke_shell) : SmokeShell {
 class tbd_mortars_105mm_shell_ammo_smoke : SubmunitionCore {
     submunitionAmmo = QGVAR(105mm_smoke_shell);
     submunitionDirectionType = "SubmunitionModelDirection";
-    submunitionConeType[] = { "poissondisc", 3 };  // 20
-    submunitionConeAngle = 180;
-    submunitionConeAngleHorizontal = 360;
+    // submunitionConeType[] = { "poissondisc", 3 };  // 20
+    // submunitionConeAngle = 180;
+    // submunitionConeAngleHorizontal = 360;
+    submunitionConeAngle = 15;
+    submunitionConeType[] = { "random", 3 };
     submunitionInitialOffset[] = { 0, 0, 0 };
     deleteParentWhenTriggered = 0;
     submunitionParentSpeedCoef = 0.95;
