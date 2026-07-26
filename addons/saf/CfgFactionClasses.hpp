c4cc7bb0b1acaf96b5e15b800f7a9c1781808e1b
abridgford <bridgforda@hotmail.co.uk>
2026-07-26 12:36:14 +0100

    add flag

--- addons/saf/CfgFactionClasses.hpp
+++ addons/saf/CfgFactionClasses.hpp
@@ -1,6 +1,7 @@
 class CfgFactionClasses {
     class ADDON {
         displayName = "Sumava Armed Forces";
+        icon = QPATHTOF(data\SAF_Flag.paa);
         priority = 1;
         side = 1;
     };
